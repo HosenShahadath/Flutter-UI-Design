f88ebc7df6a467a95d193aae13f4cbfe9ca15bc0
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-05-05 13:49:24 +0600

    recipe app

--- lib/Food Recipe App/Views/home_screen_recipe.dart
+++ lib/Food Recipe App/Views/home_screen_recipe.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_design/Food%20Recipe%20App/Colors/color.dart';
+import 'package:flutter_ui_design/Food%20Recipe%20App/models/categories_recipe.dart';
 import 'package:flutter_ui_design/Food%20Recipe%20App/models/recipe_model.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -12,6 +13,13 @@ class HomeScreenRecipe extends StatefulWidget {
 
 class _HomeScreenRecipeState extends State<HomeScreenRecipe> {
   int selectedIndex = 0;
+  int selectedPage = 0;
+  List<IconData> icons = [
+    Icons.home,
+    Icons.bar_chart_rounded,
+    Icons.heart_broken,
+    Icons.person_rounded,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -66,8 +74,81 @@ class _HomeScreenRecipeState extends State<HomeScreenRecipe> {
                           borderRadius: BorderRadius.circular(8),
                           image: DecorationImage(
                             image: AssetImage(recipe.image),
-                            fit: BoxFit.cover
+                            fit: BoxFit.cover,
                           ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: recipe.fav ? Colors.red : Colors.black45,
+                              ),
+                              child: const Icon(
+                                Icons.heart_broken,
+                                color: Colors.white,
+                                size: 18,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.black45,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          recipe.name,
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            height: 0,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                      const Icon(
+                                        Icons.star,
+                                        color: Colors.amberAccent,
+                                        size: 18,
+                                      ),
+                                      Text(
+                                        "${recipe.rate}",
+                                        style: const TextStyle(
+                                          height: 0,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 3),
+                                  Text(
+                                    "1 Bowl (${recipe.weight}g)",
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 12,
+                                      height: 0,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 3),
+                                  Text(
+                                    "${recipe.calorie} kkal | 25% AKG",
+                                    style: TextStyle(color: Colors.white70),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -75,7 +156,153 @@ class _HomeScreenRecipeState extends State<HomeScreenRecipe> {
                 }),
               ),
             ),
+            const SizedBox(height: 40),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    "Sell all",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.green,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            categoryItems(),
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://media.istockphoto.com/id/1213660289/photo/young-beautiful-chinese-chef-woman-wearing-cooker-uniform-and-hat-holding-tray-with-dome-with.jpg?s=612x612&w=0&k=20&c=Acr3SpWXvGhElDWXTo2Z7hfc7jpUQrXJuOs9SzuZEHA=",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Hona Ci Minh",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        "Expert Chef",
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
+        ),
+      ),
+      bottomSheet: Container(
+        height: 80,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: Colors.black12, spreadRadius: 1)],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(
+            icons.length,
+            (index) => GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedPage = index;
+                });
+              },
+              child: SizedBox(
+                height: 40,
+                width: 40,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      icons[index],
+                      color: index == selectedPage ? Colors.green : Colors.grey,
+                      size: 28,
+                    ),
+                    index == selectedPage
+                        ? Container(
+                          height: 3,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                        )
+                        : const SizedBox(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView categoryItems() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          recipeCategory.length,
+          (index) => Padding(
+            padding:
+                index == 0
+                    ? const EdgeInsets.only(left: 20, right: 20)
+                    : const EdgeInsets.only(right: 20),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 33,
+                  backgroundColor: recipeCategory[index].color,
+                  child: Image.asset(
+                    recipeCategory[index].image,
+                    height: 40,
+                    width: 40,
+                  ),
+                ),
+                Text(
+                  recipeCategory[index].name,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
